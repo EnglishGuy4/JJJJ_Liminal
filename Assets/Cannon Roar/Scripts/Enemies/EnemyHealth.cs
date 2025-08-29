34325cfa9f203fe6ca46dbe582615ee56da05bbf
EnglishGuy4 <72713585+EnglishGuy4@users.noreply.github.com>
2025-08-29 16:35:56 +1000

    projectiles explode into more projectiles
    the bigger ones also take two hits to go down.
    just like minecraft slimes.

--- Assets/Cannon Roar/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Cannon Roar/Scripts/Enemies/EnemyHealth.cs
@@ -17,6 +17,10 @@ public class EnemyHealth : MonoBehaviour
     private EnemyMovement enemyMovement;
     public bool bossShip;
     public bool enemyShip;
+    public bool explodeIntoPieces;
+    public GameObject explodesInto;
+    public GameObject spherePart;
+    public Material hitMaterial;
     [HideInInspector]
     public GameObject cannonBall;
 
@@ -60,6 +64,12 @@ public class EnemyHealth : MonoBehaviour
         {
             Death();
         }
+
+        // If it's about to break, change it's colour. (Hardcoded values because this is for debugging, at least for now.)
+        if (health == 1)
+        {
+            Debug.Log("Wow! You got the hit!");
+        }
     }
 
     public void Death()
@@ -77,6 +87,28 @@ public class EnemyHealth : MonoBehaviour
         // Remove from spawner list if needed
         if (enemyShip && enemySpawnerScript != null)
             enemySpawnerScript.enemiesFromThisSpawnerList.Remove(gameObject);
+
+        // Should we explode and fly out into a million (or two) pieces?
+        if (explodeIntoPieces)
+        {
+            // Spawn 2 pieces
+            for (int i = 0; i < 2; i++)
+            {
+                GameObject shrapnel = Instantiate(
+                    explodesInto,
+                    transform.position,
+                    Quaternion.identity
+                );
+
+                Rigidbody rb = shrapnel.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // Send each one flying in opposite(ish) directions
+                    Vector3 dir = (i == 0 ? Vector3.left : Vector3.right) + Vector3.up;
+                    rb.AddForce(dir.normalized * 5f, ForceMode.Impulse);
+                }
+            }
+        }
 
         // Deactivate instead of destroying
         gameObject.SetActive(false);
