2d19103d47b3b474003e230ca61e599ce05fa97a
EnglishGuy4 <72713585+EnglishGuy4@users.noreply.github.com>
2025-09-08 12:51:48 +1000

    Flying Enemies
    - There are a few enemy ships in the EnemyTest scene that fly to the left and to the right.
    - Ships will turn around when they hit the borders on the left and right, which should be far enough that the player does not see this happen.
    - Ships will shoot stuff at whatever their target is at an interval, with varying accuracy. To give the illusion that they're fighting eachother, this is just somewhere above the city at the moment.
    - I accidentally discovered that enemy ships will fly off-course when you shoot them, due to being rigidbodys. This is now a feature. They will fly off for a few seconds before exploding (which I have not implemented yet).

---
+++ Assets/Scripts/EnemyBullets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyBullets : MonoBehaviour
+{
+    public float despawnTime = 20f;
+    float time = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If it's time to go, then go we shall.
+        time += Time.deltaTime;
+        if (time > despawnTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
